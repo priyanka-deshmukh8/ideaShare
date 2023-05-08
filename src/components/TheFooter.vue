fa0063c30ab87585986ed9722e45845f77b42f2a
priyanka-deshmukh8 <priyankadeshmukh.h@gmail.com>
2023-05-08 09:51:51 +0530

    vue2

---
+++ src/components/TheFooter.vue
@@ -0,0 +1,18 @@
+<template>
+  <footer>
+    <div class="container">
+      <router-link class="logo-font" :to="{ name: 'home', params: {} }">
+        ideaShare
+      </router-link>
+      <span class="attribution">
+        An interactive learning project 
+      </span>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "RwvFooter"
+};
+</script>
